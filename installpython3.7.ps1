0feac7cba10d62d636f670abcce9b2fb9ed1df81
asawalhi <45780806+asawalhi@users.noreply.github.com>
2021-07-20 00:00:40 -0500

    Create installpython3.7.ps1

---
+++ installpython3.7.ps1
@@ -0,0 +1,5 @@
+cd \
+mkdir py
+cd py
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/py/python-3.7.0.exe"
+C:\py\python-3.7.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_pip=1
