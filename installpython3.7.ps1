7fedde1663650af772fe340a774963958345e076
asawalhi <45780806+asawalhi@users.noreply.github.com>
2021-07-20 14:38:46 -0500

    Update installpython3.7.ps1

--- installpython3.7.ps1
+++ installpython3.7.ps1
@@ -3,3 +3,4 @@ mkdir py
 cd py
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/py/python-3.7.0.exe"
 C:\py\python-3.7.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_pip=1
+##
