31abc27c17717283fc95047d9ba29d3d3900d2f6
asawalhi <45780806+asawalhi@users.noreply.github.com>
2021-07-21 15:10:10 -0500

    Update downloadpython3.7.ps1

--- downloadpython3.7.ps1
+++ downloadpython3.7.ps1
@@ -1,5 +1,12 @@
+powershell
 cd \
 mkdir py
 cd py
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/py/python-3.7.0.exe"
+
+powershell
+Start-Process "C:\py\python-3.7.0.exe" -argumentlist "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_pip=1" -Wait 
+
+invoke-expression 'cmd /c start powershell -command {pip install pandas}' 
+
 ##
