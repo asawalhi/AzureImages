dcf98199b8b994b4d3f799e0c1eb13b33b526c92
asawalhi <45780806+asawalhi@users.noreply.github.com>
2021-07-27 19:20:41 +0300

    Update downloadpython3.7.ps1

--- downloadpython3.7.ps1
+++ downloadpython3.7.ps1
@@ -5,5 +5,4 @@ Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe
 
 Start-Process "C:\py\python-3.7.0.exe" -argumentlist "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_pip=1" -Wait 
 
-cd c:\buildArtifacts\ImageBuilder\
 invoke-expression 'cmd /c start powershell -command {pip install -r requirements.txt}' 
