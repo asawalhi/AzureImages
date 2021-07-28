d5aaf03f85b479dea2bef9d2994cf1663b8778dc
asawalhi <45780806+asawalhi@users.noreply.github.com>
2021-07-28 05:56:38 +0300

    Update downloadpython3.7.ps1

--- downloadpython3.7.ps1
+++ downloadpython3.7.ps1
@@ -6,6 +6,6 @@ Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0-amd
 Start-Process "C:\py\python-3.7.0.exe" -argumentlist "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0 Include_pip=1" -Wait 
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-invoke-expression 'cmd /c start powershell -command {pip install pandas==0.25}' 
+invoke-expression 'cmd /c start powershell -command {pip install wheel}' 
 
 pip list
