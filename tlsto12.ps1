b687f1684515762c9d5dc6a1b714676182de49df
asawalhi <45780806+asawalhi@users.noreply.github.com>
2021-09-11 18:14:35 -0500

    Update tlsto12.ps1

--- tlsto12.ps1
+++ tlsto12.ps1
@@ -1,1 +1,3 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module PowerShellGet -RequiredVersion 2.2.4 -SkipPublisherCheck -Force
